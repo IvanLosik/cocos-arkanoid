2a8fc0d9bd222513fb0c0a11db2532f99233650f
Ivan <losikivan2002@gmail.com>
2026-04-26 04:19:27 +0300

    try to change color

--- build/web-mobile/src/settings.b63a4.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",main:"e5753"}};
